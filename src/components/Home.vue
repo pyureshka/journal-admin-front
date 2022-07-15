95bbed8f82cfeff98d6ba612589ba6bf1594394a
pyureshka <polinapyura@mail.ru>
2022-07-15 09:41:34 +0900

    change: list items

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app-bar
       app
-      color="white"
+      color="#92bfbd52"
       flat
   >
     <v-container class="py-0 fill-height">
@@ -9,49 +9,26 @@
           class="mr-10"
           color="grey darken-1"
           size="32"
-      ></v-avatar>
-
-      <v-btn
-          v-for="link in links"
-          :key="link"
-          text
-      >
-        {{ link }}
-      </v-btn>
-
-      <v-spacer></v-spacer>
+      />
     </v-container>
   </v-app-bar>
-  <v-main class="grey lighten-3">
+
+  <v-main>
     <v-container>
+
       <v-row>
         <v-col cols="2">
           <v-sheet rounded="lg">
-            <v-list color="transparent">
-              <v-list-item
-                  v-for="n in 5"
-                  :key="n"
-                  link
-              >
-                <v-list-item-content>
-                  <v-list-item-title>
-                    List Item {{ n }}
-                  </v-list-item-title>
-                </v-list-item-content>
+            <v-list color="#92bfbd52" rounded="lg">
+              <v-list-item link="">
+                  <v-list-item-title>Классы</v-list-item-title>
               </v-list-item>
-
-              <v-divider class="my-2"></v-divider>
-
-              <v-list-item
-                  link
-                  color="grey lighten-4"
-              >
-                <v-list-item-content>
-                  <v-list-item-title>
-                    Refresh
-                  </v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
+	            <v-list-item link="">
+		              <v-list-item-title>Предметы</v-list-item-title>
+	            </v-list-item>
+	            <v-list-item link="">
+	              <v-list-item-title>Ученики</v-list-item-title>
+	            </v-list-item>
             </v-list>
           </v-sheet>
         </v-col>
@@ -60,11 +37,33 @@
           <v-sheet
               min-height="70vh"
               rounded="lg"
+              color="#92bfbd52"
           >
-            <!--  -->
           </v-sheet>
         </v-col>
       </v-row>
+
     </v-container>
   </v-main>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+	data() {
+		return {
+			info: null
+		};
+	},
+	mounted() {
+		axios
+				.get('')
+				.then(response => (this.info = response));
+	}
+}
+</script>
+
+<style lang="scss">
+
+</style>

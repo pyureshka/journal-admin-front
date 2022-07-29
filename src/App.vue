d2f0e2bf689a9fa0134c6a4e64e3fde5d5ecc5c4
pyureshka <polinapyura@mail.ru>
2022-07-29 15:27:51 +0900

    update: style

--- src/App.vue
+++ src/App.vue
@@ -6,31 +6,18 @@
         <q-btn dense flat round icon="mdi-menu" @click="toggleLeftDrawer"/>
 
         <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Title
+          <q-avatar></q-avatar>
+          Журнал
         </q-toolbar-title>
-
-        <q-btn dense flat round icon="mdi-menu" @click="toggleRightDrawer"/>
       </q-toolbar>
 
-      <q-tabs align="left">
-        <q-route-tab to="/page1" label="Page One"/>
-        <q-route-tab to="/page2" label="Page Two"/>
-        <q-route-tab to="/page3" label="Page Three"/>
-      </q-tabs>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+    <q-drawer v-model="leftDrawerOpen" side="left" bordered>
       <!-- drawer content -->
     </q-drawer>
 
-    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
-      <!-- drawer content -->
-    </q-drawer>
-
-    <q-page-container>
+    <q-page-container style="padding-top: 0">
       <router-view/>
     </q-page-container>
 
@@ -41,13 +28,8 @@
 import {ref} from 'vue'
 
 const leftDrawerOpen = ref(false)
-const rightDrawerOpen = ref(false)
 
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value
-}
-
-function toggleRightDrawer() {
-  rightDrawerOpen.value = !rightDrawerOpen.value
 }
 </script>

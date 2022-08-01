ad150a1270c935a0d46840529b9a5a64077587a4
pyureshka <polinapyura@mail.ru>
2022-08-01 16:31:50 +0900

    add: grade table

--- src/components/Students.vue
+++ src/components/Students.vue
@@ -6,18 +6,20 @@
                   class="col-4"
                   option-value="id"
                   :option-label="(o) => o.number + ' ' + o.liter"
-                  label="Класс"/>
+                  label="Класс"
+        />
         <q-select v-model="selectedSubject" class="col-4"
                   :options="subjects"
                   option-value="id"
                   :option-label="(o) => o.title"
-                  label="Предмет"/>
+                  label="Предмет"
+        />
 
         <q-field
             class="col-4"
             style="cursor: pointer"
             label="Период" stack-label
-            >
+        >
           <q-menu v-model="showDatePicker">
             <q-date v-model="selectedDate"
                     minimal
@@ -38,12 +40,20 @@
       <q-card class="q-ma-md" center>
         <q-card-section >
           <div class="q-pa-md">
-            <q-table
-                class="my-sticky-column-table"
-                :rows="rows"
-                :columns="columns"
-                row-key="name"
-            />
+            <q-markup-table>
+              <thead>
+              <tr>
+                <th>Фамилия Имя</th>
+                <th v-for="day in getDays(selectedDate)">{{ day }}</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr v-for="item in students" :key="item.student.id">
+                <td>{{ item.student.lastName + ' ' + item.student.firstName }}</td>
+                <td v-for="day in getDays(selectedDate)"> {{ getGrades(item,day,selectedDate) }} </td>
+              </tr>
+              </tbody>
+            </q-markup-table>
           </div>
         </q-card-section>
       </q-card>
@@ -55,7 +65,6 @@
 import {useStudents} from "../composables/useStudents";
 import {useClasses} from "../composables/useClasses";
 import {useSubjects} from "../composables/useSubjects";
-import {useGrades} from "../composables/useGrades";
 
 let selectedClass = $ref(null)
 let selectedSubject = $ref(null)
@@ -63,17 +72,35 @@ let selectedDate = $ref(null)
 let showDatePicker= $ref(false)
 let rDate = $ref(null)
 
-const {students, getClassAndGrades} = useStudents($$(selectedClass))
+const {getClassAndGrades} = useStudents($$(selectedClass))
 const {classes} = $(useClasses())
 const {subjects} = $(useSubjects($$(selectedClass)))
+let students = $ref([])
+let grade = $ref(null)
 
-const month = []
-
-// const { years, month } = $(useGrades($$(selectedYear)))
-function onDatepickerUpdate(_v,reason) {
+async function onDatepickerUpdate(_v,reason) {
   rDate.setView('Months')
   if(reason==='month') showDatePicker = false
-  getClassAndGrades(selectedClass.id,selectedSubject.id,selectedDate)
+  students = await getClassAndGrades(selectedClass.id,selectedSubject.id,selectedDate)
+}
+function getDays() {
+  let newDate = new Date(selectedDate)
+  let lastDay = new Date(newDate.getFullYear(), newDate.getMonth()+1, 0).getDate()
+  let daysArr = []
+  let c = 0
+  for(let i = 1; i <= lastDay; i++) {
+    daysArr[c] = i
+    c++
+  }
+  return daysArr
+}
+// !
+function getGrades(item, day, selectedDate) {
+  grade = item.grades.find(g => g.date === (selectedDate+'-'+day))
+  if(grade == null)
+    return '-'
+  else
+    return grade.grade
 }
 
 </script>
